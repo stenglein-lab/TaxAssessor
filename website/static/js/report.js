56bc55e4030b7fbf56aa7b47db4b52c1e8355e7a
josephtallison <josephtallison@gmail.com>
2015-11-10 11:51:16 -0700

    Refactoring code style

--- website/static/js/report.js
+++ website/static/js/report.js
@@ -1,134 +1,129 @@
-var username=$('#username').attr("value"),
-    filename=$('#openFile').attr("value");
+TaxAss.report = {}
+TaxAss.report.json = '../../docs/'+$('#username').attr('value')+
+                    '/'+$('#open-file').attr('value')+'_report.json';
 
-var reportJson = "../../docs/"+username+"/"+filename+"_report.json";
-var reportData
-
-$(document).ready(function(){
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: reportJson,
-        success: function(data) {
-            populateData(data)
-        },
-        error: function() {
-            $('#align_table_data').html("ERROR IN DISPLAYING DATA");
-        }
-    });
-});
-
-function populateData(data) {
-    reportData = data;
-    
-    var taxData = "",
-        alignData = "";
-    
-    alignData += '<tr><td>Number of Reads</td>' + "<td>" + data['nReads'] + "</td></tr>";
-    alignData += '<tr><td>Total Alignments</td>' + "<td>" + data['nAligns'] + "</td></tr>";
-    alignData += '<tr><td>Total Genes Aligned To</td>' + "<td>" + data['nGis'] + "</td></tr>";
-    alignData += '<tr><td>Fewest Alignments for a read</td>' + "<td>" + data['minAligns'] + "</td></tr>";
-    alignData += '<tr><td>Most Alignments for a read</td>' + "<td>" + data['maxAligns'] + "</td></tr>";
-    alignData += '<tr><td>Average Alignments per read</td>' + "<td>" + data['aveAligns'] + "</td></tr>";    
-    
-    $('#align_table_data').html(alignData);
-    
-    createGiBarChart(data['giBins'],data['giBinCounts']);
-    
-}
-
-
-function createGiBarChart(bins,values) {
+TaxAss.report.createGiBarChart = function(bins,values) {
    
-    var data = [];
+  var data = [];
 
-    for (var i = 0; i < bins.length; i++) {
-        data.push({key: bins[i], value: values[i]});
-    }
-    
-    var margin = {top: 20, right: 20, bottom: 70, left: 75},
+  for (var i = 0; i < bins.length; i++) {
+    data.push({key: bins[i], value: values[i]});
+  }
+  
+  var margin = {top: 20, right: 20, bottom: 70, left: 75},
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
-    var y = d3.scale.linear().range([height, 0]);
+  var y = d3.scale.linear().range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .tickValues(bins);
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .tickValues(bins);
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(10);
-        
-    Array.max = function( array ){
-        return Math.max.apply( Math, array );
-    };
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left')
+    .ticks(10);
     
-    if (Array.max(values) >= 10000) {
-        yAxis.tickFormat(d3.format("e"));
-    }
-        
-    var svg = d3.select("#gi_chart").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+  Array.max = function( array ){
+    return Math.max.apply( Math, array );
+  };
+  
+  if (Array.max(values) >= 10000) {
+    yAxis.tickFormat(d3.format('e'));
+  }
+    
+  var svg = d3.select('#gi_chart').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 
+        'translate(' + margin.left + ',' + margin.top + ')');
 
-    data.forEach(function(d) {
-        d.date = d.key;
-        d.value = +d.value;
-    });
-        
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  data.forEach(function(d) {
+    d.date = d.key;
+    d.value = +d.value;
+  });
+    
+  x.domain(data.map(function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
-        
-    d3.select(".x")
-        .append("text")
-        .attr("transform", "rotate(90)" )
-        .attr("y", -width)
-        .attr("dy", "-0.5em")
-        .attr("x", 0)
-        .attr("dx", "-0.5em")
-        .text("# GIs");
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+    .selectAll('text')
+    .style('text-anchor', 'end')
+    .attr('dx', '-.8em')
+    .attr('dy', '-.55em')
+    .attr('transform', 'rotate(-90)' );
+    
+  d3.select('.x')
+    .append('text')
+    .attr('transform', 'rotate(90)' )
+    .attr('y', -width)
+    .attr('dy', '-0.5em')
+    .attr('x', 0)
+    .attr('dx', '-0.5em')
+    .text('# GIs');
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("y", 0)
-        .attr("dy", "-0.5em")
-        .attr("x", 0)
-        .attr("dx", "0.5em")
-        .style("text-anchor", "end")
-        .text("# Reads");
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+    .append('text')
+    .attr('y', 0)
+    .attr('dy', '-0.5em')
+    .attr('x', 0)
+    .attr('dx', '0.5em')
+    .style('text-anchor', 'end')
+    .text('# Reads');
 
-    svg.selectAll("bar")
-        .data(data)
-        .enter().append("rect")
-        .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
+  svg.selectAll('bar')
+    .data(data)
+    .enter().append('rect')
+    .style('fill', 'steelblue')
+    .attr('x', function(d) { return x(d.date); })
+    .attr('width', x.rangeBand())
+    .attr('y', function(d) { return y(d.value); })
+    .attr('height', function(d) { return height - y(d.value); });
+}
+
+TaxAss.report.populateData = function(data) {
+  
+  var taxData = '',
+    alignData = '';
+  
+  alignData+='<tr><td>Number of Reads</td><td>'+data['nReads']+'</td></tr>';
+  alignData+='<tr><td>Total Alignments</td><td>'+data['nAligns']+'</td></tr>';
+  alignData+='<tr><td>Total Genes Aligned To</td><td>'+data['nGis']+
+             '</td></tr>';
+  alignData+='<tr><td>Fewest Alignments for a read</td><td>'+data['minAligns']+
+             '</td></tr>';
+  alignData+='<tr><td>Most Alignments for a read</td><td>'+data['maxAligns']+
+             '</td></tr>';
+  alignData+='<tr><td>Average Alignments per read</td><td>'+data['aveAligns']+
+             '</td></tr>';  
+  
+  $('#align-table-data').html(alignData);
+  
+  TaxAss.report.createGiBarChart(data['giBins'],data['giBinCounts']);
 }
 
 
-
-
-
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: TaxAss.report.json,
+    success: function(data) {
+      TaxAss.report.populateData(data)
+    },
+    error: function() {
+      $('#align-table-data').html('ERROR IN DISPLAYING DATA');
+    }
+  });
+});
 
