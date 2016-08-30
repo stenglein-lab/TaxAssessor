b0efc4381733e02a339ab0b35e8738fce44854b4
josephtallison <josephtallison@gmail.com>
2016-08-30 08:13:29 -0600

    tweak setup script

---
+++ setup/db_import.sql
@@ -0,0 +1,9 @@
+USE TaxAssessor_Refs;
+
+#populate the table: seqIdToTaxId_NCBI
+LOAD DATA INFILE ‘database_import/nucl_est.accession2taxid’
+INTO TABLE table seqIdToTaxId_NCBI
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+(accession, accessionVersion, taxId, gi)
+IGNORE 1 LINES;
