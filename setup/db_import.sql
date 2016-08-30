13d32c7628222d33d68afcd1eb072d4ea194b2c0
josephtallison <josephtallison@gmail.com>
2016-08-30 12:15:24 -0600

    nearing completion

--- setup/db_import.sql
+++ setup/db_import.sql
@@ -1,5 +1,5 @@
 USE TaxAssessor_Refs;
-/*
+
 #populate the table: seqIdToTaxId_NCBI
 LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_est.accession2taxid' 
 INTO TABLE seqIdToTaxId_NCBI 
@@ -35,7 +35,7 @@ FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES 
 (accession, accessionVersion, taxId, gi);
-*/
+
 #populate the table: taxIdNodes_NCBI
 LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nodes.dmp' 
 INTO TABLE taxIdNodes_NCBI 
