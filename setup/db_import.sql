b4609bd8c632783bb52529c254c4b64ba0f63642
josephtallison <josephtallison@gmail.com>
2016-08-30 11:33:34 -0600

    added taxIdNodes_NCBI infile import to script

--- setup/db_import.sql
+++ setup/db_import.sql
@@ -1,5 +1,5 @@
 USE TaxAssessor_Refs;
-
+/*
 #populate the table: seqIdToTaxId_NCBI
 LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_est.accession2taxid' 
 INTO TABLE seqIdToTaxId_NCBI 
@@ -7,3 +7,39 @@ FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES 
 (accession, accessionVersion, taxId, gi);
+
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_gb.accession2taxid' 
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES 
+(accession, accessionVersion, taxId, gi);
+
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_gss.accession2taxid' 
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES 
+(accession, accessionVersion, taxId, gi);
+
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_wgs.accession2taxid' 
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES 
+(accession, accessionVersion, taxId, gi);
+
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/prot.accession2taxid' 
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES 
+(accession, accessionVersion, taxId, gi);
+*/
+#populate the table: taxIdNodes_NCBI
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nodes.dmp' 
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t|\t' 
+LINES TERMINATED BY '\r\n' 
+IGNORE 1 LINES 
+(child, parent);
