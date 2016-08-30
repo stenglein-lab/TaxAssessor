291a4d4593317e7f7ef5e044f78fe5d9c6491b6b
josephtallison <josephtallison@gmail.com>
2016-08-30 08:24:57 -0600

    tweak setup script

--- setup/db_import.sql
+++ setup/db_import.sql
@@ -1,9 +1,9 @@
 USE TaxAssessor_Refs;
 
 #populate the table: seqIdToTaxId_NCBI
-LOAD DATA INFILE ‘database_import/nucl_est.accession2taxid’
-INTO TABLE seqIdToTaxId_NCBI
-FIELDS TERMINATED BY '\t'
-LINES TERMINATED BY '\n'
-(accession, accessionVersion, taxId, gi)
-IGNORE 1 LINES;
+LOAD DATA INFILE '/home/jallison/TaxAssessor/setup/database_import/nucl_est.accession2taxid'
+INTO TABLE seqIdToTaxId_NCBI 
+FIELDS TERMINATED BY '\t' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES 
+(accession, accessionVersion, taxId, gi);
