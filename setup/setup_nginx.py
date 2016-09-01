01a24936b21f3d576ffb5141e200dd2ba6d3bc55
josephtallison <josephtallison@gmail.com>
2016-09-01 13:39:59 -0600

    modified some nginx setup

--- setup/setup_nginx.py
+++ setup/setup_nginx.py
@@ -12,10 +12,10 @@ import os
 if __name__ == "__main__":
     taxAssDir = os.environ['TaxAssessor']
     with open("../nginx/conf/new.conf","w") as outFile:
-        with open("../nginx/conf/nginx.conf","r") as inFile:
+        with open("../nginx/conf/setup_nginx.conf","r") as inFile:
             for line in inFile:
                 outFile.write(line.replace('{TaxAssessor}',taxAssDir))
-    os.rename("../nginx/conf/nginx.conf","../nginx/conf/setup_nginx.conf")
+    os.rename("../nginx/conf/nginx.conf","../nginx/conf/nginx_save.conf")
     os.rename("../nginx/conf/new.conf","../nginx/conf/nginx.conf")
     
 
